5d3bbdc8fcff86cb031c370682dd3b40d76204f3
canuckistani <jeff@canuckistani.ca>
2015-12-14 16:05:48 -0800

    implemented table flip

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ const { data } = require('sdk/self');
 const fs = require('sdk/io/fs');
 const { Hotkey } = require("sdk/hotkeys");
 const clipboard = require("sdk/clipboard");
-const emoji = {sup: "¯\\_(ツ)_/¯"};
 const notifications = require("sdk/notifications");
 
-// first run prefs
-const prefs = require("sdk/simple-prefs").prefs;
+// data
+const emoji = {
+  shrug: "¯\\_(ツ)_/¯",
+  flip: "(╯°□°）╯︵ ┻━┻"
+};
 
-let showHotKey = Hotkey({
+// first run prefs
+// const prefs = require("sdk/simple-prefs").prefs;
+
+let shrugHotKey = Hotkey({
   combo: "accel-shift-s",
   onPress: function() {
-    console.log('ran hotkey');
-    // panel.show();
-    clipboard.set(emoji.sup);
-
+    clipboard.set(emoji.shrug);
     notifications.notify({
       title: "Shrug-tastic!!",
-      text: emoji.sup + " is ready to deploy!",
-      onClick: function (data) {
-        console.log(data);
-        // console.log(this.data) would produce the same result.
-      }
+      text: emoji.shrug + " is ready to deploy!"
+    });
+  }
+});
+
+let flipHotKey = Hotkey({
+  combo: "accel-shift-f",
+  onPress: function() {
+    clipboard.set(emoji.flip);
+    notifications.notify({
+      title: "Shrug-tastic!!",
+      text: emoji.flip + " is ready to deploy!"
     });
   }
 });
@@ -32,9 +41,7 @@ module.exports = {
 
 // first run experience
 
-if (!prefs.hasBeenRun) {
-
-  // prefs.hasBeenRun = true;
-  console.log('firstrun hasn\'t been run');
-
-}
+// if (!prefs.hasBeenRun) {
+//   // prefs.hasBeenRun = true;
+//   console.log('firstrun hasn\'t been run');
+// }
